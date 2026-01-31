7e4591aa1c4cd6eb31bca8eccb86a2e99a0c5479
NB-LNV15I7-6112\Adminit <Akkarapon.k@vri.co.th>
2026-02-01 02:39:47 +0700

    Fix vite config imports

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'iceberg-js': '/src/lib/mock-iceberg.js'
+      'iceberg-js': path.resolve(__dirname, './src/lib/mock-iceberg.js')
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ['iceberg-js'],
-    },
-  },
-  optimizeDeps: {
-    exclude: ['iceberg-js'],
   },
   build: {
     rollupOptions: {
